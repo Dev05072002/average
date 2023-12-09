21491d4d5d5f4a38923fd1fe806c82007cf585e7
Dev05072002 <152999395+Dev05072002@users.noreply.github.com>
2023-12-09 11:51:06 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c,avg;
+    printf("enter the values of the variables");
+    scanf("\n%d%d%d",&a,&b,&c);
+    avg=(a+b+c)/3;
+    printf("\n the average of the given numbers are%d",avg);
+    getch();
+    
+}
